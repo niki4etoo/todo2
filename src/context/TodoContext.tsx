982ee53dafc55bf527c29110d0e5ec8730fd6c19
Nikolai Nanev <niki4etooo@gmail.com>
2023-11-27 08:20:41 +0200

    added update, delete, edit functionalities

--- src/context/TodoContext.tsx
+++ src/context/TodoContext.tsx
@@ -2,16 +2,21 @@ import React, { createContext, useState } from 'react'
 import { nanoid } from 'nanoid'
 import { useLocalStorage } from 'usehooks-ts'
 
+interface TodoContextProps {
+  todos: Todo[]
+  addTodo: (text: string) => void
+  deleteTodo: (id: string) => void
+  editTodo: (id: string, text: string) => void
+  updateTodoStatus: (id: string) => void
+}
+
 export interface Todo {
   id: string
   text: string
   status: 'undone' | 'completed'
 }
 
-interface TodoContextProps {
-  todos: string[]
-  addTodo: (text: string) => void
-}
+
 
 
 export const TodoContext = createContext<TodoContextProps | undefined>(undefined, )
@@ -20,6 +25,7 @@ export const TodoProvider = (props: { children: React.ReactNode }) => {
   
   const [todos, setTodos ] = useState<Todo[]>([])
 
+  // ::: ADD METHOD :::
   const addTodo = (text: string) => {
     const newTodo: Todo = {
       id: nanoid(),
@@ -29,9 +35,44 @@ export const TodoProvider = (props: { children: React.ReactNode }) => {
     setTodos([...todos, newTodo])
   }
   
+  // ::: DELETE METHOD :::
+  const deleteTodo = (id: string) => {
+    setTodos(prevTodos => prevTodos.filter(todo => todo.id !== id))
+  }
+
+  // ::: EDIT METHOD :::
+  const editTodo = (id: string, text: string) => {
+    setTodos(prevTodos => {
+      return prevTodos.map(todo => {
+        if(todo.id === id){
+          return { ...todo, text }
+        }
+        return todo
+      })
+    })
+  }
+
+  // ::: UPDATE METHOD :::
+  const updateTodoStatus = (id: string) => {
+    setTodos(prevTodos => {
+      return prevTodos.map(todo => {
+        if(todo.id === id){
+          return {
+            ...todo,
+            status: todo.status === 'undone' ? 'completed' : 'undone',
+          }
+        }
+        return todo
+      })
+    })
+  }
+
   const value: TodoContextProps = {
     todos,
     addTodo,
+    deleteTodo,
+    editTodo,
+    updateTodoStatus,
   }
 
   return (
